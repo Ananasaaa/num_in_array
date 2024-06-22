30ae7a968e1ea0c648b3a6ed3ad014640df58a4b
Ananasaaa <kairyakn@gmail.com>
2024-06-22 18:46:26 +0300

    num_in_array

---
+++ script.js
@@ -0,0 +1,20 @@
+const arr = [2, 11, 34, 65, 41, 8, 26, 12, 9, 54];
+const newArr = [];
+
+for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+       newArr.push(arr[i]);
+    }
+}
+
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
